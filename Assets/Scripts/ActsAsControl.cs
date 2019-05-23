1b1112d6b061afc534ec86919f757df52e063b1a
Rodney Degracia <rdegraci@gmail.com>
2019-05-22 21:57:42 -0400

    Fix files

---
+++ Assets/Scripts/ActsAsControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActsAsControl : MonoBehaviour
+{
+    public float speed = 0f;
+    float t;
+    Vector3 startPosition;
+    Vector3 target;
+    float timeToReachTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = target = transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+        if (target != transform.position)
+        {
+            t += Time.deltaTime / timeToReachTarget;
+            transform.position = Vector3.Lerp(startPosition, target, t);
+        }
+    }
+
+    public void SetDestination(Vector3 destination, float time)
+    {
+        t = 0;
+        startPosition = transform.position;
+        timeToReachTarget = time;
+        target = destination;
+    }
+
+    public void Rotate(float speed)
+    {
+        this.speed = speed;
+    }
+}
